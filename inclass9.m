8cc5569ce4e8f9a9eea0f00f7d66a46357fd796b
Aryeh <warmflasha@gmail.com>
2017-09-26 13:39:08 -0500

    Wrote inclass9

---
+++ inclass9.m
@@ -0,0 +1,10 @@
+% Inclass assignment 9
+
+% The accession number for human NOTCH1 mRNA is AF308602
+% 1. Read the information from this entry into matlab
+% 2. Write code that runs a blast query on the first 500 base pairs of this
+% gene against the refseq_rna database
+% 3. Find the three highest scoring hits from other species and identify
+% the length of the alignment and fraction of matches/mismatches. 
+% 4. Run the same query against the database est_human. Comment on the
+% sequences that you find. 
